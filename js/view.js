4238c3e12cfb5579850ca2c5c0f5df242b32f0f5
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-04-17 18:45:52 -0500

    Addin filters

--- js/view.js
+++ js/view.js
@@ -1,5 +1,6 @@
 import AddTodo from "./components/add-todo.js";
 import Modal from "./components/modal.js";
+import Filters from './components/filters.js';
 
 export default class View {
 	constructor() {
@@ -14,10 +15,40 @@ export default class View {
 
 		this.modal = new Modal();
 		this.modal.onClick((id, values) => this.editTodo(id, values));
+
+		this.filters = new Filters();
+		this.filters.onClick((filters) => this.filter(filters));
 	}
 
 	setModel(model) {
 		this.model = model;
+	}
+
+
+	filter(filters) {
+		const {type, words} = filters;
+		const [, ...rows] = this.table.getElementsByTagName('tr');
+		for (const row of rows) {
+			const [title, description, completed] = row.children;
+			let shouldHide = false;
+
+			if (words) {
+				shouldHide = !title.innerText.includes(words) && !description.innerText.includes(words);
+			}
+
+			const shouldBeCompleted = type === 'completed';
+			const isCompleted = completed.children[0].checked;
+
+			if (type !== 'all' && shouldBeCompleted !== isCompleted) {
+				shouldHide = true;
+			}
+
+			if (shouldHide) {
+				row.classList.add('d-none');
+			} else {
+				row.classList.remove('d-none');
+			}
+		}
 	}
 
 	render() {
@@ -73,7 +104,12 @@ export default class View {
 		editBtn.innerHTML = '<i class="fa fa-pencil"></i>';
 		editBtn.setAttribute('data-toggle', 'modal');
 		editBtn.setAttribute('data-target', '#modal');
-		editBtn.onclick = () => this.modal.setValues(todo);
+		editBtn.onclick = () => this.modal.setValues({
+			id: todo.id,
+			title: row.children[0].innerText,
+			description: row.children[1].innerText,
+			completed: row.children[2].children[0].checked,
+		});
 		// adding remove button to row
 		row.children[3].appendChild(editBtn);
 
